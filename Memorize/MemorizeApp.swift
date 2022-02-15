f1b65a4cd8e085e0163cd2fd0f3641743f0154dd
fabiano <silva@appratico.com>
2022-02-15 19:49:30 -0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Fabiano Silva on 15/02/22.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
